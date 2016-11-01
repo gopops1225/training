126d3d6799b895ce984a638dd41922090cbc078a
Lionel Clark <gopops1225@gmail.com>
2016-11-01 09:01:04 -0500

    service

---
+++ MonsterWeb/MonsterWeb.Client/DataService.cs
@@ -0,0 +1,20 @@
+﻿using MonsterWeb.logic.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonsterWeb.Client
+{
+  
+  public class DataService
+  {
+    private MonsterServiceClient msc = new MonsterServiceClient();
+
+    public async void GetGenders()
+    {
+      await msc.GetGendersAsync();
+    }
+
+  }
+}
---
+++ MonsterWeb/MonsterWeb.logic/TestData.cs
@@ -0,0 +1,21 @@
+﻿using MonsterApp.DataClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterWeb.logic
+{
+  public class TestData
+  {
+    private MonsterService data = new MonsterService();
+
+    public void Getenders()
+    {
+      data.GetGenders();
+    }
+
+
+  }
+}
